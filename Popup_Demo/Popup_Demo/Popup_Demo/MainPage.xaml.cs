5f74786b1c04df02db1c1d4de9fe3a3d71a2c82d
shivagurunathan.kamalakannan <shivagurunathan.kamalakannan@syncfusion.com>
2019-04-02 23:28:22 +0530

    UG sample has been committed.

---
+++ Popup_Demo/Popup_Demo/Popup_Demo/MainPage.xaml.cs
@@ -0,0 +1,37 @@
+﻿using Syncfusion.XForms.PopupLayout;
+using System;
+using Xamarin.Forms;
+
+namespace Popup_Demo
+{
+    public partial class MainPage : ContentPage
+    {
+        SfPopupLayout popupLayout;
+        DataTemplate dataTemplate;
+        public MainPage()
+        {
+            InitializeComponent();
+            this.popupLayout = new SfPopupLayout();
+            this.dataTemplate = new DataTemplate(() =>
+            {
+                Label label = new Label();
+                label.Text = "Syncfusion";
+                label.BackgroundColor = Color.Transparent;
+                return label;
+            });
+
+            this.popupLayout.PopupView.HeaderTitle = "Company name";
+            this.popupLayout.PopupView.ContentTemplate = dataTemplate;
+
+            //// You can customize your color based on the requirement.
+            this.popupLayout.PopupView.BackgroundColor = Color.LightBlue;
+            this.clickToShowPopup.Clicked += ClickToShowPopup_Clicked;
+        }
+
+        private void ClickToShowPopup_Clicked(object sender, EventArgs e)
+        {
+            //// Opens SfPopupLayout.
+            this.popupLayout.Show();
+        }
+    }
+}

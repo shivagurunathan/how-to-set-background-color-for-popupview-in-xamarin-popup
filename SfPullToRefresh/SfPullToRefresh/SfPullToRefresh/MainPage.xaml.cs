0aa659bcc95b35ce1508472f7a58885eb2498831
shivagurunathan.kamalakannan <shivagurunathan.kamalakannan@syncfusion.com>
2019-04-05 15:13:37 +0530

    SfPullToRefresh has been committed.

---
+++ SfPullToRefresh/SfPullToRefresh/SfPullToRefresh/MainPage.xaml.cs
@@ -0,0 +1,101 @@
+﻿using Syncfusion.SfPullToRefresh.XForms;
+using Syncfusion.XForms.Border;
+using Syncfusion.XForms.ProgressBar;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace SfPullToRefresh
+{
+    public partial class MainPage : ContentPage
+    {
+        private SfCircularProgressBar progressbar;
+        private SfBorder border;
+        public MainPage()
+        {
+            InitializeComponent();
+            this.progressbar = new SfCircularProgressBar();
+            this.border = new SfBorder();
+
+
+            this.border.BorderColor = Color.LightGray;
+            this.border.BackgroundColor = Color.White;
+            this.border.CornerRadius = 35;
+            this.border.Content = this.progressbar;
+            this.border.BorderWidth = 0.2;
+
+            this.progressbar.SegmentCount = 10;
+            this.progressbar.IndicatorInnerRadius = 0.5;
+            this.progressbar.IndicatorOuterRadius = 0.7;
+            this.progressbar.ShowProgressValue = true;
+            this.progressbar.GapWidth = 0.5;
+            this.progressbar.WidthRequest = 70;
+            this.progressbar.HeightRequest = 55;
+            this.progressbar.IndeterminateAnimationDuration = 750;
+
+            var pullingTemplate = new DataTemplate(() =>
+            {
+                return new ViewCell { View = this.border };
+            });
+
+            this.pullToRefresh.Refreshing += this.PullToRefresh_Refreshing;
+            this.pullToRefresh.Pulling += this.PullToRefresh_Pulling;
+
+            this.pullToRefresh.PullingViewTemplate = pullingTemplate;
+            this.pullToRefresh.RefreshingViewTemplate = pullingTemplate;
+        }
+
+        private async void pullToRefresh_Refreshing(object sender, EventArgs e)
+        {
+            pullToRefresh.IsRefreshing = true;
+            await Task.Delay(new TimeSpan(0, 0, 3));
+            viewModel.ItemsSourceRefresh();
+            pullToRefresh.IsRefreshing = false;
+        }
+
+        private void PullToRefresh_Pulling(object sender, PullingEventArgs e)
+        {
+            this.progressbar.TrackInnerRadius = 0.8;
+            this.progressbar.TrackOuterRadius = 0.1;
+            this.progressbar.IsIndeterminate = false;
+            this.progressbar.ProgressColor = Color.FromRgb(0, 124, 238);
+            this.progressbar.TrackColor = Color.White;
+
+            var absoluteProgress = Math.Abs(e.Progress);
+            this.progressbar.Progress = absoluteProgress;
+            this.progressbar.SetProgress(absoluteProgress, 1, Easing.CubicInOut);
+        }
+
+        private async void PullToRefresh_Refreshing(object sender, EventArgs e)
+        {
+            this.pullToRefresh.IsRefreshing = true;
+            await this.AnimateRefresh();
+
+            this.progressbar.TrackInnerRadius = 0.1;
+            this.progressbar.TrackOuterRadius = 0.9;
+
+            this.viewModel.ItemsSourceRefresh();
+            this.pullToRefresh.IsRefreshing = false;
+        }
+
+        private async Task AnimateRefresh()
+        {
+            this.progressbar.Progress = 0;
+            this.progressbar.IsIndeterminate = true;
+
+            await Task.Delay(750);
+            this.progressbar.ProgressColor = Color.Red;
+
+            await Task.Delay(750);
+            this.progressbar.ProgressColor = Color.Green;
+
+            await Task.Delay(750);
+            this.progressbar.ProgressColor = Color.Orange;
+
+            await Task.Delay(750);
+        }
+    } 
+}
---
+++ SfPullToRefresh/SfPullToRefresh/SfPullToRefresh/ViewModel/OrderInfoRepository.cs
@@ -0,0 +1,109 @@
+﻿using Syncfusion.Data.Extensions;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SfPullToRefresh
+{
+    class OrderInfoRepository : NotificationObject
+    {
+
+        private ObservableCollection<OrderInfo> orderInfo;
+        private int _pageSize;
+
+        private int count = 1045;
+
+        public ObservableCollection<OrderInfo> OrderInfoCollection
+        {
+            get { return orderInfo; }
+            set { this.orderInfo = value; }
+        }
+
+        public int PageSize
+        {
+            get { return _pageSize; }
+            set { this._pageSize = value; }
+        }
+        public OrderInfoRepository()
+        {
+            orderInfo = new ObservableCollection<OrderInfo>();
+            PageSize = 20;
+            this.GenerateOrders();
+        }
+
+        private void GenerateOrders()
+        {
+            for (int i = 1; i <= 5; i++)
+            {
+                orderInfo.Add(new OrderInfo(1001, "Maria Anders", "Mexico", "ANATR", "Mexico D.F.", true));
+                orderInfo.Add(new OrderInfo(1002, "Ana Trujillo", "Mexico", "ANTON", "Mexico D.F.", true));
+                orderInfo.Add(new OrderInfo(1003, "Ant Fuller", "UK", "AROUT", "London", true));
+                orderInfo.Add(new OrderInfo(1004, "Thomas Hardy", "Sweden", "BERGS", "Berlin", true));
+                orderInfo.Add(new OrderInfo(1005, "Lenny Lin", "Germany", "BLAUS", "Mannheim", true));
+                orderInfo.Add(new OrderInfo(1006, "John Carter", "France", "BLONP", "Strasbourg", true));
+                orderInfo.Add(new OrderInfo(1007, "Laura King", "Spain", "BOLID", "Madrid", true));
+                orderInfo.Add(new OrderInfo(1008, "Hanna Moos", "France", "BONAP", "Marseille", false));
+                orderInfo.Add(new OrderInfo(1009, "Lenny Lin", "Canada", "BOTTM", "Tsawassen", false));
+                orderInfo.Add(new OrderInfo(1010, "Hanna Moos", "UK", "AROUT", "London", false));
+                orderInfo.Add(new OrderInfo(1011, "John Carter", "Germany", "BLAUS", "Mannheim", false));
+                orderInfo.Add(new OrderInfo(1012, "Tim Adams", "France", "BLONP", "Strasbourg", true));
+                orderInfo.Add(new OrderInfo(1013, "Hanna Moos", "UK", "AROUT", "London", false));
+                orderInfo.Add(new OrderInfo(1014, "John Carter", "Germany", "ALFKI", "Berlin", true));
+                orderInfo.Add(new OrderInfo(1015, "Hanna Moos", "Mexico", "ANATR", "Mexico D.F.", false));
+                orderInfo.Add(new OrderInfo(1016, "Laura King", "Mexico", "ANTON", "Mexico D.F.", false));
+                orderInfo.Add(new OrderInfo(1017, "Lenny Lin", "UK", "AROUT", "London", false));
+                orderInfo.Add(new OrderInfo(1018, "Thomas Hardy", "Sweden", "BERGS", "Berlin", true));
+                orderInfo.Add(new OrderInfo(1019, "Lenny Lin", "Germany", "BLAUS", "Mannheim", true));
+                orderInfo.Add(new OrderInfo(1020, "Gina Irene", "France", "BLONP", "Strasbourg", true));
+                orderInfo.Add(new OrderInfo(1021, "Laura King", "Spain", "BOLID", "Madrid", false));
+                orderInfo.Add(new OrderInfo(1022, "Anne Wilson", "France", "BONAP", "Marseille", true));
+                orderInfo.Add(new OrderInfo(1023, "Lenny Lin", "Canada", "BOTTM", "Tsawassen", false));
+                orderInfo.Add(new OrderInfo(1024, "Gina Irene", "UK", "AROUT", "London", true));
+                orderInfo.Add(new OrderInfo(1025, "Thomas Hardy", "Germany", "BLAUS", "Mannheim", true));
+                orderInfo.Add(new OrderInfo(1026, "Anne Wilson", "France", "BLONP", "Strasbourg", false));
+                orderInfo.Add(new OrderInfo(1027, "Laura King", "UK", "AROUT", "London", true));
+                orderInfo.Add(new OrderInfo(1028, "Anne Wilson", "France", "BLONP", "Strasbourg", true));
+                orderInfo.Add(new OrderInfo(1029, "Gina Irene", "UK", "AROUT", "London", false));
+                orderInfo.Add(new OrderInfo(1030, "Anne Wilson", "Germany", "ALFKI", "Berlin", false));
+                orderInfo.Add(new OrderInfo(1031, "Tim Adams", "Mexico", "ANATR", "Mexico D.F.", true));
+                orderInfo.Add(new OrderInfo(1032, "Anne Wilson", "Mexico", "ANTON", "Mexico D.F.", true));
+                orderInfo.Add(new OrderInfo(1033, "Gina Irene", "UK", "AROUT", "London", false));
+                orderInfo.Add(new OrderInfo(1034, "Ant Fuller", "Sweden", "BERGS", "Berlin", true));
+                orderInfo.Add(new OrderInfo(1035, "Thomas Hardy", "Germany", "BLAUS", "Mannheim", false));
+                orderInfo.Add(new OrderInfo(1036, "Gina Irene", "France", "BLONP", "Strasbourg", true));
+                orderInfo.Add(new OrderInfo(1037, "Hanna Moos", "Spain", "BOLID", "Madrid", false));
+                orderInfo.Add(new OrderInfo(1038, "Anne Wilson", "France", "BONAP", "Marseille", false));
+                orderInfo.Add(new OrderInfo(1039, "Thomas Hardy", "Canada", "BOTTM", "Tsawassen", true));
+                orderInfo.Add(new OrderInfo(1040, "Tim Adams", "UK", "AROUT", "London", true));
+                orderInfo.Add(new OrderInfo(1041, "Anne Wilson", "Germany", "BLAUS", "Mannheim", true));
+                orderInfo.Add(new OrderInfo(1042, "Thomas Hardy", "France", "BLONP", "Strasbourg", false));
+                orderInfo.Add(new OrderInfo(1043, "Hanna Moos", "UK", "AROUT", "London", true));
+                orderInfo.Add(new OrderInfo(1044, "John Carter", "Germany", "ALFKI", "Berlin", false));
+            }
+        }
+
+        public void ItemsSourceRefresh()
+        {
+            orderInfo.Insert(0, new OrderInfo(1101, "Maria Anders", "Mexico", "ANATR", "Mexico D.F.", true));
+            orderInfo.Insert(1, new OrderInfo(1102, "Ana Trujillo", "Mexico", "ANTON", "Mexico D.F.", true));
+            orderInfo.Insert(2, new OrderInfo(1103, "Ant Fuller", "UK", "AROUT", "London", true));
+            orderInfo.Insert(3, new OrderInfo(1104, "Thomas Hardy", "Sweden", "BERGS", "Berlin", true));
+            orderInfo.Insert(4, new OrderInfo(1105, "Lenny Lin", "Germany", "BLAUS", "Mannheim", true));
+        }
+    }
+
+    public class NotificationObject : INotifyPropertyChanged
+    {
+        public void RaisePropertyChanged(string propName)
+        {
+            if (this.PropertyChanged != null)
+                this.PropertyChanged(this, new PropertyChangedEventArgs(propName));
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
